f8fb0a89ccaf81db7b10bb3dd083d2e13222d4f9
Connor-John <connor.taylor@icloud.com>
2020-06-08 20:09:50 +1200

    update for data preprocessing

--- preprocessing.py
+++ preprocessing.py
@@ -43,15 +43,83 @@ def get_labels(files, N):
     
     label_count = Counter(labels)
 
-    return label_count
+    return labels, label_count
+
+# create train/test split of data
+def create_datasets(img, n_subjects, N, images, labels):
+    
+    # Train on 3 images per subject
+    n_test = 3 * n_subjects
+    n_train = N - n_test
+
+    # initialise datasets
+    train_images = np.zeros([n_train] + list(img.shape))
+    train_labels = np.zeros([n_train])
+
+    test_images = np.zeros([n_test] + list(img.shape))
+    test_labels = np.zeros([n_test])
+
+    # populate the datasets
+    count = {}
+    train_i = 0
+    test_i = 0
+
+    for img, label in zip(images, labels):
+    
+        count[label] = count.get(label, 0) + 1
+
+        if count[label] > 3:
+
+            train_images[train_i] = img
+            train_labels[train_i] = label
+
+            train_i += 1
+        
+        else:
+
+            test_images[test_i] = img
+            test_labels[test_i] = label
+
+            test_i += 1
+    
+    return train_images, train_labels, test_images, test_labels
+
+# Create dictionary of training data
+# {Data label :  indexes of data}
+def create_dictionary(train_labels, test_labels):
+    
+    train_label2idx = {}
+    test_label2idx = {} 
+
+    for i, label in enumerate(train_labels):
+        if label not in train_label2idx:
+            train_label2idx[label] = [i]
+        else:
+            train_label2idx[label].append(i)
+
+    for i, label in enumerate(test_labels):
+        if label not in test_label2idx:
+            test_label2idx[label] = [i]
+        else:
+            test_label2idx[label].append(i)
+    
+    return train_label2idx, test_label2idx
 
 # prepare data
-def prep_data(datapath, H, W):
+def prepare_data(datapath, H, W):
 
     files = get_data(datapath)
-    # img = load_img(filepath, H, W)
+    
+    img = load_img(np.random.choice(files), H, W)
     N = len(files)
     images = image_array(files, N, H, W)
-    label_count = get_labels(files, N)
+
+    labels, label_count = get_labels(files, N)
+    unique_labels = set(label_count.keys())
+    n_subjects = len(label_count)
+    
+    train_images, train_labels, test_images, test_labels = create_datasets(img, n_subjects, N, images, labels)
+
+    train_label2idx, test_label2idx = create_dictionary(train_labels, test_labels)
 
 
